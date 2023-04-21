2ed9287b98c3eeebb8509d8a20ee2da36317f5b9
Ihor <ihormalyi34@gmail.com>
2023-04-21 13:34:11 +0200

    delete abstract methods

--- method.py
+++ method.py
@@ -6,13 +6,3 @@ class Method(ABC):
     @abstractmethod
     def solution(self):
         pass
-
-    @abstractmethod
-    def generate_chart(self):
-        """Generate chart and return chart object"""
-
-    @abstractmethod
-    def step_number(self) -> int:
-        """Return step number"""
-        pass
-
