c4929d94a162708d2600c249d60f3b7ba9c57ea8
Ihor <ihormalyi34@gmail.com>
2023-04-21 12:34:24 +0200

    add main; add methods to newton.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from newton import Newton
+import math
+
+
+def key(x):
+    return math.log(x*x, math.e) - math.sin(x) - 2
+
+def main():
+    n = Newton(key, 10^-5)
+
+
+if __name__ == "__main__":
+    main()
--- newton.py
+++ newton.py
@@ -4,3 +4,12 @@ from method import Method
 class Newton(Method):
     def __init__(self, key: callable, e: float) -> None:
         pass
+
+    def solution(self):
+        return 0
+
+    def generate_chart(self):
+        return 0
+
+    def step_number(self) -> int:
+        return 0
