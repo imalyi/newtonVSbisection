b1045f6f43c4e04069aefa35cfb64ffde758954f
SheeyeDev <102896476+SheeyeDev@users.noreply.github.com>
2023-04-28 12:42:38 +0200

    Znajdowanie wszystkich pierwiastków bisekcją, menu

--- main.py
+++ main.py
@@ -6,11 +6,44 @@ import math
 def key(x):
     return math.log(x*x, math.e) - math.sin(x) - 2
 
+def menu():
+    print("Menu:\n")
+    print("1. Znajdź pierwiastek")
+    print("2. Znajdź wszystkie pierwiastki")
+    print("3. Wyjdź")
+
+def findRoot():
+    print("Wpisz e")
+    e = float(input())
+    print("Wpisz a (początek przedziału)")
+    a = float(input())
+    print("Wpisz b (koniec przedziału)")
+    b = float(input())
+    bisect = Bisection(key, [a, b], e)
+    answer = bisect.solution()
+    print("Bisekcja: Pierwiastek równania to ", answer[0], ",obliczone w ", answer[1], " krokach.")
+
+def findAllRoots(a,b,precision):
+    c = a
+    while c<=b:
+        c += precision
+        if key(a)*key(c)<0:
+            bisect = Bisection(key,[a,c],math.pow(10,-8))
+            answer = bisect.solution()
+            print("Bisekcja: Pierwiastek równania to ", answer[0], ",obliczone w ", answer[1], " krokach.")
+            a = c
 
 def main():
-    b = Bisection(key,[-2.5,-1],math.pow(10,-5))
-    answer=b.solution()
-    print("Bisection: Answer is ",answer[0]," in ",answer[1]," steps")
+    cont = True
+    while(cont):
+        menu()
+        choice = int(input())
+        if choice == 1:
+            findRoot()
+        if choice == 2:
+            findAllRoots(-10,10,0.001)
+        if choice == 3:
+            cont = False
 
 
 if __name__ == "__main__":
